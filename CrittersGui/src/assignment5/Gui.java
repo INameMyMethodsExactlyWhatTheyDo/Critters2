6f5b61198b69640fb7878b6400b77766b6e48df8
Kyle Zhou <Kyle.a.zhou@gmail.com>
2018-04-03 00:03:05 -0500

    WHoa thats a lot of stuff

--- CrittersGui/src/assignment5/Gui.java
+++ CrittersGui/src/assignment5/Gui.java
@@ -1,9 +1,9 @@
+package assignment5;
 
 public class Gui {
 	 public static void main(String[] args) {
 
 		 System.out.println("hello world~~~");
-
 		 System.out.println("hello world");
 		 //edit 
 		 }
