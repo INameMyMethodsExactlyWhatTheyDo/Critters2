07d5477593411ae208b5ad2856baef19dd56572f
Kyle Zhou <kyle.a.zhou@gmail.com>
2018-04-03 10:26:13 -0500

    Some gui examples

--- CrittersGui/src/assignment5/Gui.java
+++ CrittersGui/src/assignment5/Gui.java
@@ -1,10 +1,72 @@
 package assignment5;
 
-public class Gui {
-	 public static void main(String[] args) {
+import java.util.*;
+import javafx.*;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.event.*;
 
-		 System.out.println("hello world~~~");
-		 System.out.println("hello world");
-		 //edit 
-		 }
+public class Gui extends Application{
+
+    @Override
+    public void start(Stage stage){
+    	Pane p1 = new Pane();
+    	Pane p2 = new Pane();
+    	Scene s1 = new Scene(p1, 800, 600);
+        Scene s2 = new Scene(p2, 500, 300);        
+    	Button b1 = new Button("Click me ");
+    	Button b2 = new Button("click me again");
+    	Label l1 = new Label("SCence 1");
+    	Label l2 = new Label("SCecnene 2");
+    	p1.getChildren().add(b1);
+    	p1.getChildren().add(l1);
+    	l1.setFont(new Font(28));
+    	l1.setLayoutY(100);
+    	l1.setLayoutX(400);
+    	l1.setMaxSize(200, 200);
+    	b1.setFont(new Font(28));
+    	b1.setMaxSize(200,200);
+    	
+    	p2.getChildren().add(b2);
+    	p2.getChildren().add(l2);
+    	l2.setFont(new Font(20));
+    	l2.setLayoutY(50);
+    	l2.setLayoutX(50);
+    	l2.setMaxSize(200, 200);
+    	b2.setFont(new Font(20));
+    	b2.setLayoutX(100);
+    	b2.setMaxSize(200,200);
+    	
+    	b1.setOnAction(new EventHandler<ActionEvent>() {
+    		@Override
+    		public void handle(ActionEvent e) {
+    			stage.setScene(s2);
+    		}
+    	});
+    	
+    	b2.setOnAction(new EventHandler<ActionEvent>() {
+    	    @Override public void handle(ActionEvent e) {
+    	    	stage.setScene(s1);
+    	    }
+    	});
+    	
+        stage.setTitle("My app");
+        stage.setScene(s1);
+        stage.show();
+        
+    }
+    
+    public static void main(String[] args){
+        Application.launch(args);
+
+    }
 }
