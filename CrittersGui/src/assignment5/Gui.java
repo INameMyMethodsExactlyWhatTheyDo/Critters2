08a0bf32e45398f12deb40bcec43e6cb8cfe3df7
Karen Yen <karencyen@gmail.com>
2018-04-03 21:45:37 -0500

    grid stuff

--- CrittersGui/src/assignment5/Gui.java
+++ CrittersGui/src/assignment5/Gui.java
@@ -7,10 +7,15 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.transform.Translate;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.event.*;
@@ -19,12 +24,23 @@ public class Gui extends Application{
 
     @Override
     public void start(Stage stage){
+    	Circle circEx = new Circle();
+    	circEx.setCenterX(150.0f); 
+    	circEx.setCenterY(135.0f); 
+    	circEx.setRadius(100.0f);
+    	
     	Pane p1 = new Pane();
     	Pane p2 = new Pane();
-    	Scene s1 = new Scene(p1, 800, 600);
+    	GridPane test = new GridPane();
+  
+//    	Scene s1 = new Scene(p1, 800, 600);
+    	Scene s1 = new Scene(test, 800, 600);
         Scene s2 = new Scene(p2, 500, 300);        
     	Button b1 = new Button("Click me ");
     	Button b2 = new Button("click me again");
+    	Button b3 = new Button("Stats stuff");
+    	b3.setLayoutY(100);
+    	p1.getChildren().add(b3);
     	Label l1 = new Label("SCence 1");
     	Label l2 = new Label("SCecnene 2");
     	p1.getChildren().add(b1);
@@ -34,7 +50,7 @@ public class Gui extends Application{
     	l1.setLayoutX(400);
     	l1.setMaxSize(200, 200);
     	b1.setFont(new Font(28));
-    	b1.setMaxSize(200,200);
+    	b1.setMaxSize(100,100);
     	
     	p2.getChildren().add(b2);
     	p2.getChildren().add(l2);
@@ -45,17 +61,63 @@ public class Gui extends Application{
     	b2.setFont(new Font(20));
     	b2.setLayoutX(100);
     	b2.setMaxSize(200,200);
+    	List<Circle> testPop = new java.util.ArrayList<Circle>();
     	
+    	test.setGridLinesVisible(true);
+    	
+    	for (int i = 0; i < Params.world_height; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / Params.world_height);
+            test.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < Params.world_width; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / Params.world_width);
+            test.getRowConstraints().add(rowConst);         
+        }
+        
+        test.add(b1, 1, 1);
+        
+    	//button adds and moves the circles
+        //we can make this step
     	b1.setOnAction(new EventHandler<ActionEvent>() {
     		@Override
     		public void handle(ActionEvent e) {
-    			stage.setScene(s2);
+    			//stage.setScene(s2);
+    			Circle circEx = new Circle();
+    	    	circEx.setCenterX(Math.random()*1600); 
+    	    	circEx.setCenterY(Math.random()*800); 
+    	    	circEx.setRadius(10.0f);
+    			test.add(circEx, 0, 4);
+    			testPop.add(circEx);
+    			
+    			for(int i = 0; i < testPop.size(); i++) {
+    				int x = Critter.getRandomInt(2);
+    				int y = Critter.getRandomInt(2);
+
+    				int xCo = test.getRowIndex(testPop.get(i));
+    				int yCo = test.getColumnIndex(testPop.get(i));
+
+    				test.setColumnIndex(testPop.get(i), yCo + y );
+    				test.setRowIndex(testPop.get(i), xCo + x);
+
+    	    		
+    	    	}
     		}
     	});
     	
     	b2.setOnAction(new EventHandler<ActionEvent>() {
     	    @Override public void handle(ActionEvent e) {
     	    	stage.setScene(s1);
+    	    
+    	    }
+    	});
+    	
+    	b3.setOnAction(new EventHandler<ActionEvent>() {
+    	    @Override public void handle(ActionEvent e) {
+    	    	
+    	    	//stage.setScene(s1);
+    	    
     	    }
     	});
     	
