aa7e3f8c0c76517578dade7a1445b82ed9b0712f
Karen Yen <karencyen@gmail.com>
2018-04-03 22:57:06 -0500

    resizable canvas

--- CrittersGui/src/assignment5/Gui.java
+++ CrittersGui/src/assignment5/Gui.java
@@ -5,6 +5,8 @@ import javafx.*;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
@@ -12,6 +14,8 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -123,7 +127,49 @@ public class Gui extends Application{
     	
         stage.setTitle("My app");
         stage.setScene(s1);
-        stage.show();
+        //stage.show();
+        
+        
+        
+        /////////////////////////canvas stuff///////////
+
+        
+        //primaryStage.setTitle("Drawing Operations Test");
+        Group root = new Group();
+        Canvas canvas = new Canvas(300, 250);
+        
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawShapes(gc);
+
+
+      // test.add(canvas, 5, 5);
+       // stage.show();
+        
+        ///////////////////////////////
+//        ResizableCanvas can = new ResizableCanvas();
+//        
+//        GridPane resizePane = new GridPane();
+//        resizePane.getChildren().add(can);
+//     
+//        // Bind canvas size to stack pane size.
+//        can.widthProperty().bind(resizePane.widthProperty());
+//        can.heightProperty().bind(resizePane.heightProperty());
+//     
+//        stage.setScene(new Scene(resizePane));
+//        stage.setTitle("Tip 1: Resizable Canvas");
+//        stage.show();
+       ResizableCanvas can = new ResizableCanvas();
+       
+       GridPane resizePane = new GridPane();
+       test.getChildren().add(can);
+    
+       // Bind canvas size to stack pane size.
+       can.widthProperty().bind(test.widthProperty());
+       can.heightProperty().bind(test.heightProperty());
+    
+       stage.setScene(s1);
+       stage.setTitle("Tip 1: Resizable Canvas");
+       stage.show();
         
     }
     
@@ -131,4 +177,65 @@ public class Gui extends Application{
         Application.launch(args);
 
     }
+
+
+    private void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+//        gc.setStroke(Color.BLUE);
+//        gc.setLineWidth(5);
+//        gc.strokeLine(40, 10, 10, 40);
+        //gc.fillOval(w*5, h*5, 30, 30);
+        gc.fillOval(10, 60, 30, 30);
+       // gc.fillOval
+//        gc.strokeOval(60, 60, 30, 30);
+//        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+//        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+//        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+//        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+//        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+//        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+//        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+//        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+//        gc.fillPolygon(new double[]{10, 40, 10, 40},
+//                       new double[]{210, 210, 240, 240}, 4);
+//        gc.strokePolygon(new double[]{60, 90, 60, 90},
+//                         new double[]{210, 210, 240, 240}, 4);
+//        gc.strokePolyline(new double[]{110, 140, 110, 140},
+//                          new double[]{210, 210, 240, 240}, 4);
+    }
+    class ResizableCanvas extends Canvas {
+    	 
+        public ResizableCanvas() {
+          // Redraw canvas when size changes.
+          widthProperty().addListener(evt -> draw());
+          heightProperty().addListener(evt -> draw());
+        }
+     
+        private void draw() {
+          double width = getWidth();
+          double height = getHeight();
+     
+          GraphicsContext gc = getGraphicsContext2D();
+          gc.clearRect(0, 0, width, height);
+     
+          gc.setStroke(Color.RED);
+          gc.strokeLine(0, 0, width, height);
+          gc.strokeLine(0, height, width, 0);
+        }
+     
+        @Override
+        public boolean isResizable() {
+          return true;
+        }
+     
+        @Override
+        public double prefWidth(double height) {
+          return getWidth();
+        }
+     
+        @Override
+        public double prefHeight(double width) {
+          return getHeight();
+        }
+      }
 }
